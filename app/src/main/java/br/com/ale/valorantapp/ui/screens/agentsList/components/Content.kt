e14101722c419cfb9a1c2a3821416649f9cbd278
Alexandre Fernandes <alefernandeseng@gmail.com>
2024-01-24 20:50:52 -0300

    fix: padding in Content Composable function

--- app/src/main/java/br/com/ale/valorantapp/ui/screens/agentsList/components/Content.kt
+++ app/src/main/java/br/com/ale/valorantapp/ui/screens/agentsList/components/Content.kt
@@ -1,16 +1,20 @@
 package br.com.ale.valorantapp.ui.screens.agentsList.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import br.com.ale.composefirstapp.utils.UiEvent
 import br.com.ale.valorantapp.models.AgentsModel
 
+
 @Composable
 fun Content(agents: List<AgentsModel>, onClickItem: (UiEvent.Navigate) -> Unit) {
-    LazyColumn {
+    LazyColumn(modifier = Modifier.padding(horizontal = 8.dp)) {
         items(agents) { agent ->
-            AgentItem(agent, onClickItem)
+                AgentItem(agent, onClickItem)
         }
     }
 }
